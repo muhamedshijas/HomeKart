dc0638cc3928ffed489e0ee9cd553fd4c675966a
muhamedshijas <shijushijas157@gmail.com>
2023-02-27 11:22:03 +0530

    OTP section make responsive

--- models/BannerModel.js
+++ models/BannerModel.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose")
-
 const bannerSchema = new mongoose.Schema({
    name:String,
    banner:String,
 
 });
-
 const  BannerModel = mongoose.model('banners', bannerSchema);
 module.exports=BannerModel;
 
