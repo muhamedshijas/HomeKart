0c684c271c4402ad38801e4439b8699913aed765
muhamedshijas <shijushijas157@gmail.com>
2023-07-25 11:25:26 +0530

    url updated normally

--- dbConnect.js
+++ dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect=()=>{
     mongoose.set('strictQuery', true);
-    mongoose.connect('mongodb+srv://shijas:muhamedShijas@cluster0.n9am9so.mongodb.net/')
+    mongoose.connect('mongodb+srv://muhamedshijas:Wildhunter2.0@cluster0.nfsaofu.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('Connected!')).catch(err=>{
         console.log("error : ", err)
     })
